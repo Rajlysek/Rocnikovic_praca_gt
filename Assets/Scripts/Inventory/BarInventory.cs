3ae61ef6bae23285755982a678a3ba39ef3f411a
Rajlysek <vanessa.peslova@seznam.cz>
2026-03-31 14:01:55 +0200

    bar fixing

--- Assets/Scripts/Inventory/BarInventory.cs
+++ Assets/Scripts/Inventory/BarInventory.cs
@@ -7,52 +7,129 @@ public class BarInventory : MonoBehaviour
     
     public GameObject[] SelectedSlots;
     public GameObject[] UnselectedSlots;
+    public ActionsOfPlayer actionsOfPlayer;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        for (int i = 0; i < SelectedSlots.Length; i++)
+        {
+            UnselectedSlots[i].SetActive(true);
+            
+        }
+        DeselectAllSlots();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.anyKeyDown)
+        {
+            WaitForButton();
+
+        }
        
     }
     public void DeselectAllSlots()
     {
         for (int i = 0; i < SelectedSlots.Length; i++)
         {
-            SelectedSlots[i].SetActive(false);
             UnselectedSlots[i].SetActive(true);
+            SelectedSlots[i].SetActive(false);
         }
     }
     public void WaitForButton()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+       
+        if (Input.GetKeyDown(KeyCode.Alpha1))   
         {
-            
-            SelectedSlots[0].SetActive(true);
-            UnselectedSlots[0].SetActive(false);
+           
+            if (SelectedSlots[0].activeSelf)
+            {
+                DeselectAllSlots();
+                Debug.Log("Slot 1 is already selected");
+                
+                actionsOfPlayer.ItemID = 0;
+            }
+            else
+            {
+                DeselectAllSlots();
+                SelectedSlots[0].SetActive(true);
+                
+                actionsOfPlayer.ItemID = 1;
+                Debug.Log("sELECTING 1");
+            }
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-           
-            SelectedSlots[1].SetActive(true);
-            UnselectedSlots[1].SetActive(false);
+            
+
+            if (SelectedSlots[1].activeSelf)
+            {
+                DeselectAllSlots();
+                Debug.Log("Slot 2 is already selected");
+                
+                
+                actionsOfPlayer.ItemID = 0;
+
+            }
+            else
+            {
+                DeselectAllSlots();
+                Debug.Log("sELECTING 2");
+
+                SelectedSlots[1].SetActive(true);
+                
+                actionsOfPlayer.ItemID = 2;
+            }
         }
         else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-          
-            SelectedSlots[2].SetActive(true);
-            UnselectedSlots[2].SetActive(false);
+           
+
+            if (SelectedSlots[2].activeSelf)
+            {
+                DeselectAllSlots();
+                Debug.Log("Slot 3 is already selected");
+                
+               
+                actionsOfPlayer.ItemID = 0;
+
+
+            }
+            else
+            {
+                Debug.Log("sELECTING 3");
+                DeselectAllSlots();
+                SelectedSlots[2].SetActive(true);
+                
+                actionsOfPlayer.ItemID = 3;
+            }
         }
         else if (Input.GetKeyDown(KeyCode.Alpha4))
         {
-       
-            SelectedSlots[3].SetActive(true);
-            UnselectedSlots[3].SetActive(false);
+           
+
+            if (SelectedSlots[3].activeSelf)
+            {
+                DeselectAllSlots();
+                Debug.Log("Slot 4 is already selected");
+
+           
+                
+                actionsOfPlayer.ItemID = 0;
+
+
+            }
+            else
+            {
+                DeselectAllSlots();
+                Debug.Log("sELECTING 4");
+                SelectedSlots[3].SetActive(true);
+                
+                actionsOfPlayer.ItemID = 4;
+            }
         }
     }
 }
--- Assets/Scripts/Inventory/BarInventorySlot.cs
+++ Assets/Scripts/Inventory/BarInventorySlot.cs
@@ -2,28 +2,29 @@ using UnityEngine;
 
 public class BarInventorySlot : MonoBehaviour
 {
-   
-  
+    
+    private BarInventory barInventory;
     private BarInventorySelected barInventorySelected;
     private BarInventoryUnselected barInventoryUnselected;  
   
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        barInventory = GameObject.Find("BarInventory").GetComponent<BarInventory>();
         barInventorySelected = GameObject.Find("SelectedSlots").GetComponent<BarInventorySelected>();
         barInventoryUnselected = GameObject.Find("UnselectedSlots").GetComponent<BarInventoryUnselected>();
-       
+        barInventory.DeselectAllSlots();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKeyDown) 
-        {
-            barInventorySelected.WaitForButton();
-            barInventoryUnselected.WaitForButton();
-        }
+        //if (Input.anyKeyDown) 
+        //{
+        //    barInventory.WaitForButton();
+            
+        //}
        
     }
 }
